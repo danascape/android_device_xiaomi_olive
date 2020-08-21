6c5420395858b10009c26f3d409acbe43b13ffba
Bruno Martins <bgcngm@gmail.com>
2020-08-21 12:22:06 +0530

    olive: consider lineage_olive product via the AOSP way
    Change-Id: Iab7b6c4d2817af88a133945df8fcc7fb964e35e9

---
+++ lineage_olive.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from olive device
+$(call inherit-product, device/xiaomi/olive/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Set shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Device identifiers.
+PRODUCT_NAME := lineage_olive
+PRODUCT_DEVICE := olive
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Redmi 8
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
