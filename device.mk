40dff434846a2e3abe539b030ea3834340a2d942
danascape <saalimquadri1@gmail.com>
2020-12-05 16:10:27 +0530

    olive: Import sensor config
    Change-Id: I8257c3992d1064cf38604a07ccaf64d4c564dcac

--- device.mk
+++ device.mk
@@ -339,6 +339,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Telephony
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
